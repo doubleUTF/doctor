be9dba46be968762e258a334e4a6d0a9604bca56
Aculau <aculau@gmail.com>
2016-07-15 18:11:21 -0700

    Updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp=require('gulp');
 var browserSync= require('browser-sync').create();
 var jshint= require('gulp-jshint');
 var stylish= require('jshint-stylish');
+var usemin= require('gulp-usemin');
+var uglify= require('gulp-uglify');
+var minifyHtml= require('gulp-minify-html');
+var minifyCss= require('gulp-minify-css');
+var imageMin= require('gulp-imagemin');
+var rev= require('gulp-rev');
+var del =require('del');
+var cache= require('gulp-cache');
+var notify = require('gulp-notify');
 
 
 // jshint Lint task
@@ -13,6 +22,30 @@ gulp.task('jshint', function(){
 
 // Default task
 gulp.task('default',['serve']);
+
+// Delete task- configured to delete dist folder
+gulp.task('clean',function(){
+  return del(['dist']);
+});
+
+// Usemin task to minify js,css,html and add revision to files
+gulp.task('usemin',['clean','jshint','imagemin'], function(){
+  return gulp.src('./*html')
+  .pipe(usemin({
+    css: [minifyCss(),rev()],
+    html: [minifyHtml({empty:true})],
+    js: [uglify(),rev()]
+  }))
+  .pipe(gulp.dest('dist/'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+     return del(['dist/img']), gulp.src('img/**')
+    .pipe(cache(imageMin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
 
 // Browsersync Watch html files
 gulp.task('serve', function(){
